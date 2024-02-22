cb397eaeea528c494418f7d1d31efd4835f9c129
Ariel Oppenheimer <arieloppenheimer@outlook.cl>
2024-02-22 18:08:46 +0000

    fix:BD and script

--- element.sh
+++ element.sh
@@ -6,15 +6,15 @@ then
 else
     if [[ $1 =~ ^[0-9]+$ ]]
     then
-      RESULT_ELEMENT="$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) WHERE atomic_number=$1")"
+      RESULT_ELEMENT="$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")"
     else
-      RESULT_ELEMENT="$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) WHERE symbol='$1' OR name='$1'")"
+      RESULT_ELEMENT="$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")"
     fi
     if [[ -z $RESULT_ELEMENT ]]
     then
       echo -e "I could not find that element in the database."
     else
-      echo $RESULT_ELEMENT|while IFS="|" read ATOMIC_NUMBER SYMBOL NAME TYPE ATOMIC_MASS MELTING BOILING TYPE_ID
+      echo $RESULT_ELEMENT|while IFS="|" read TYPE_ID ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING BOILING TYPE
       do
         echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
       done
